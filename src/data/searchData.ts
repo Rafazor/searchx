80588ebe99ec616944e0441a29c55cb929b1a11a
Rafa Calin <rafa.calin@yahoo.com>
2023-09-24 13:20:16 +0300

    Added mock data

--- src/data/searchData.ts
+++ src/data/searchData.ts
@@ -1,22 +1,158 @@
 import { ISearchElement } from '../modules/shared/types/index.ts';
+import { uuidv4 } from '../modules/shared/helpers/index.ts';
 
 export const MOCK_DATA: ISearchElement[] = [
     {
-        id: 1,
-        title: 'Test',
-        description: 'Description 1',
-        url: 'https://www.google.com',
+        id: uuidv4(),
+        title: 'React',
+        description:
+            'React is a free and open-source front-end JavaScript library for building user interfaces based on components. It is maintained by Meta and a community of individual developers and companies. React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js.',
+        url: 'https://react.dev/',
     },
     {
-        id: 2,
-        title: 'Dodo',
-        description: 'Description 2',
-        url: 'https://www.google.com',
+        id: uuidv4(),
+        title: 'React (software)',
+        description:
+            'React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library[3][4] for building user interfaces based on components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies.',
+        url: 'https://en.wikipedia.org/wiki/React_(software)',
     },
     {
-        id: 3,
-        title: 'Title 3',
-        description: 'Description 3',
-        url: 'https://www.google.com',
+        id: uuidv4(),
+        title: 'React Redux | React Redux',
+        description:
+            "React Redux is maintained by the Redux team, and kept up-to-date with the latest APIs from Redux and React. ; Designed to work with React's component model.",
+        url: 'https://react-redux.js.org/',
+    },
+    {
+        id: uuidv4(),
+        title: 'React Native · Learn once, write anywhere',
+        description:
+            'Create native apps for Android, iOS, and more using React. React Native combines the best parts of native development with React.',
+        url: 'https://reactnative.dev/',
+    },
+    {
+        id: uuidv4(),
+        title: 'React - Learn ReactJS - React Tutorial',
+        description:
+            'In this React course, you’ll build powerful interactive applications with one of the most popular JavaScript libraries.',
+        url: 'https://www.codecademy.com/learn/react-101',
+    },
+    {
+        id: uuidv4(),
+        title: 'React MUI: The React component library you always wanted',
+        description:
+            'MUI provides a simple, customizable, and accessible library of React components. Follow your own design system, or start with Material Design.',
+        url: 'https://mui.com/',
+    },
+    {
+        id: uuidv4(),
+        title: 'REACT | English meaning - Cambridge Dictionary',
+        description:
+            "She slapped him and called him names, but he didn't react. The judge reacted angrily to the suggestion that it hadn't been a fair trial.",
+        url: 'https://dictionary.cambridge.org/dictionary/english/react',
+    },
+    {
+        id: uuidv4(),
+        title: 'TypeScript: JavaScript With Syntax For Types',
+        description:
+            'TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale. Try TypeScript Now.',
+        url: 'https://www.typescriptlang.org/',
+    },
+    {
+        id: uuidv4(),
+        title: 'TypeScript',
+        description:
+            'TypeScript is a free and open-source high-level programming language developed by Microsoft that adds static typing with optional type annotations to JavaScript.',
+        url: 'https://en.wikipedia.org/wiki/TypeScript',
+    },
+    {
+        id: uuidv4(),
+        title: 'TypeScript Introduction',
+        description: 'TypeScript is JavaScript with added syntax for types.',
+        url: 'https://www.w3schools.com/typescript/typescript_intro.php',
+    },
+    {
+        id: uuidv4(),
+        title: 'TypeScript Tutorial',
+        description:
+            'TypeScript is JavaScript with added syntax for types. Start learning TypeScript now',
+        url: 'https://www.w3schools.com/typescript/',
+    },
+    {
+        id: uuidv4(),
+        title: 'TypeScript vs. JavaScript',
+        description:
+            'Considering you can include as much plain JavaScript as you like in your TypeScript project, what factors should you consider when choosing TypeScript for your application? Let’s explore.',
+        url: 'https://www.toptal.com/typescript/typescript-vs-javascript-guide',
+    },
+    {
+        id: uuidv4(),
+        title: 'TypeScript - The Ultimate Beginners Guide',
+        description:
+            'TypeScript has become increasingly popular over the last few years, and many jobs are now requiring developers to know TypeScript.',
+        url: 'https://www.freecodecamp.org/news/learn-typescript-beginners-guide/',
+    },
+    {
+        id: uuidv4(),
+        title: 'JavaScript Online',
+        description:
+            'JavaScript.com is a resource for the JavaScript community. You will find resources and examples for JavaScript beginners as well as support for JavaScript experts.',
+        url: 'https://www.javascript.com/',
+    },
+    {
+        id: uuidv4(),
+        title: 'JavaScript Tutorial',
+        description:
+            "JavaScript is the world's most popular programming language.",
+        url: 'https://www.w3schools.com/js/',
+    },
+    {
+        id: uuidv4(),
+        title: 'JavaScript',
+        description:
+            'JavaScript often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.',
+        url: 'https://en.wikipedia.org/wiki/JavaScript',
+    },
+    {
+        id: uuidv4(),
+        title: 'JavaScript basics - Learn web development | MDN',
+        description:
+            'JavaScript is a powerful programming language that can add interactivity to a website. It was invented by Brendan Eich.',
+        url: 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics',
+    },
+    {
+        id: uuidv4(),
+        title: 'JavaScript — Dynamic client-side scripting',
+        description:
+            'JavaScript is a programming language that allows you to implement complex functionalities on web pages.',
+        url: 'https://developer.mozilla.org/en-US/docs/Learn/JavaScript',
+    },
+    {
+        id: uuidv4(),
+        title: 'Java - Browse Tutorials & Online Courses | Pluralsight',
+        description:
+            'Dev & IT Certification training online. Interactive courses & practice tests. Start now! Mobile Apps. Hands-on Learning. Expert-Led Courses. Skill Assessments. Free Trial.',
+        url: 'https://www.pluralsight.com/browse/software-development/java',
+    },
+    {
+        id: uuidv4(),
+        title: 'Java IDE - Use IntelliJ IDEA',
+        description:
+            'A customizable and cross-platform IDE for Java and Kotlin development. Intelligent coding assistance, a rich set of built-in tools, and superb framework support. Top frameworks support. Profiling tools. Customizable IDE. ',
+        url: 'https://www.jetbrains.com/idea/',
+    },
+    {
+        id: uuidv4(),
+        title: 'Java | Oracle',
+        description:
+            'Get started with Java today. ... Get Java for desktop applications. Download Java · What is Java? Uninstall help.',
+        url: 'https://www.java.com/en/',
+    },
+    {
+        id: uuidv4(),
+        title: 'Java (programming language)',
+        description:
+            'Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.',
+        url: 'https://en.wikipedia.org/wiki/Java_(programming_language)',
     },
 ];
